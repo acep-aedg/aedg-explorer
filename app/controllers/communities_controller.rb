c5a898ef446fd81ac49425a5a6d027ae3cc7d67a
nicolemah99 <nicolemah99@gmail.com>
2025-01-27 14:07:22 -0900

    Order dropdown communities alphabetically

--- app/controllers/communities_controller.rb
+++ app/controllers/communities_controller.rb
@@ -1,21 +1,24 @@
 class CommunitiesController < ApplicationController
   before_action :set_community, only: %i[ show ]
+  before_action :set_sorted_communities, only: %i[ show index ]
 
   # GET /communities or /communities.json
   def index
-    @communities = Community.all
   end
 
   # GET /communities/1 or /communities/1.json
   def show
     @community = Community.find(params[:id])
-    @communities = Community.all
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_community
       @community = Community.find(params[:id])
+    end
+
+    def set_sorted_communities
+      @communities = Community.all.order(:name)
     end
 
     # Only allow a list of trusted parameters through.
