d3063ef1821d53c1fa2738be83185bfd620fe183
Will Fisher <whfisher@alaska.edu>
2025-03-04 11:51:04 -0900

    Unbreaking css compilation
    It turns out that we still need the yarn for handling our css bundling
    
    This also means that if you install a javascript library that needs CSS
    you will also need to include it using `yarn add LIBRARY` and the normal
    additions to the `application.bootstrap.css` file
    
    Signed-off-by: Will Fisher <whfisher@alaska.edu>

--- config/importmap.rb
+++ config/importmap.rb
@@ -5,7 +5,8 @@ pin "@hotwired/turbo-rails", to: "turbo.min.js"
 pin "@hotwired/stimulus", to: "stimulus.min.js"
 pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
 pin_all_from "app/javascript/controllers", under: "controllers"
-pin "bootstrap", to: "bootstrap.min.js"
+pin "bootstrap", to: 'bootstrap.min.js' # @5.3.0
 pin "datatables.net" # @2.2.2
 pin "jquery" # @3.7.1
 pin "datatables.net-bs5" # @2.2.2
+pin "@popperjs/core", to: "popper-lib.js" # @2.11.8
