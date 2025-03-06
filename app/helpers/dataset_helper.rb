bcecc235c53590578d6f29cac0c10ed287eefe23
nicolemah99 <nicolemah99@gmail.com>
2025-03-06 11:25:11 -0900

    Add dataset helper with placeholder datasets to display

---
+++ app/helpers/dataset_helper.rb
@@ -0,0 +1,11 @@
+module DatasetHelper
+  def placeholder_datasets
+    [
+      { name: "Power Cost Equalization", description: "Description/Meta Data" },
+      { name: "Fuel Prices", description: "Description/Meta Data" },
+      { name: "Energy Consumption", description: "Description/Meta Data" },
+      { name: "Solar Panel Efficiency", description: "Description/Meta Data" },
+      { name: "Wind Energy Output", description: "Description/Meta Data" }
+    ]
+  end
+end
