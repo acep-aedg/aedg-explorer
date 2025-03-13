6dc25921b11cafe5f5e237a37bd335d41185bf48
nicolemah99 <nicolemah99@gmail.com>
2025-03-13 14:47:50 -0800

    Set endpoint for production_monthly_data and create chart as an example

--- app/controllers/communities/charts_controller.rb
+++ app/controllers/communities/charts_controller.rb
@@ -1,3 +1,23 @@
 class Communities::ChartsController < ApplicationController
+  before_action :set_community
+
+  def production_monthly_data
+    grouped_data = @community.grid.monthly_generations.group(:year, :month).sum(:net_generation_mwh)
+    numeric_months = @community.grid.monthly_generations.pluck(:month).uniq.sort
+    labels = numeric_months.map { |month| Date::ABBR_MONTHNAMES[month] }
+
+    dataset = numeric_months.map do |month|
+      total_mwh = grouped_data.select { |(_, m), _| m == month }.values.sum
+      [Date::ABBR_MONTHNAMES[month], total_mwh] # Chartkick expects [Label, Value] format
+    end.to_h
+
+    render json: dataset
+  end
+
+  # Figure out if we can utilize this method from CommunitiesController instead of duplicating it here
+  private
+    def set_community
+        @community = Community.find(params[:community_id])
+    end
 end
 
