f4d0ffc44ec40ef2c25aa5888e07998453c0377f
nicolemah99 <nicolemah99@gmail.com>
2025-03-20 10:00:58 -0800

    Move stats logic to model, store grid name in variable

--- app/models/monthly_generation.rb
+++ app/models/monthly_generation.rb
@@ -3,4 +3,28 @@ class MonthlyGeneration < ApplicationRecord
   belongs_to :grid
 
   validates :grid_id, uniqueness: { scope: [:year, :fuel_type, :month], message: "combination of grid_id, year, month, and fuel type must be unique" }
+
+  scope :yearly_stats, -> (year) {
+    where(year: year).group(:month).sum(:net_generation_mwh)
+  }
+
+  def self.latest_year
+    maximum(:year)
+  end
+
+  def self.generation_stats(year = latest_year)
+    monthly_data = yearly_stats(year)
+    max_generation = monthly_data.values.max || 0
+    min_generation = monthly_data.values.min || 0
+    avg_generation = (monthly_data.values.sum.to_f / monthly_data.size).round(2) if monthly_data.any?
+
+    {
+      max_generation: max_generation,
+      min_generation: min_generation,
+      avg_generation: avg_generation,
+      max_month: monthly_data.key(max_generation),
+      min_month: monthly_data.key(min_generation),
+      latest_year: year
+    }
+  end
 end
