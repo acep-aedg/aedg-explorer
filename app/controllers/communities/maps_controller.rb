54f222c278f1fd41a5fd23c1181825fdbbea7c96
nicolemah99 <nicolemah99@gmail.com>
2025-03-21 13:36:54 -0800

    Add house districts map via community/maps controller endpoint

---
+++ app/controllers/communities/maps_controller.rb
@@ -0,0 +1,22 @@
+class Communities::MapsController < ApplicationController
+  before_action :set_community
+
+  def house_districts
+    districts = @community.house_districts.map do |district|
+      {
+        geometry: RGeo::GeoJSON.encode(district.boundary),
+        label: "District #{district.district}",
+        tooltip: district.name
+      }
+    end
+
+    render json: districts
+  end
+  
+  # Figure out if we can utilize this method from CommunitiesController instead of duplicating it here
+  private
+    def set_community
+        @community = Community.find(params[:community_id])
+    end
+end
+
