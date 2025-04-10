f60990addd2e569eed8bbf65e7990d7385f2bdb4
nicolemah99 <nicolemah99@gmail.com>
2025-04-10 15:45:26 -0800

    Test datasets controller, add test files to test download action

--- test/controllers/datasets_controller_test.rb
+++ test/controllers/datasets_controller_test.rb
@@ -1,15 +1,24 @@
 require "test_helper"
 
 class DatasetsControllerTest < ActionDispatch::IntegrationTest
-
-  test "should get index" do
-    get datasets_url
-    assert_response :success
+  setup do
+    @metadatum = oemetadata(:one)
+    @dataset = datasets(:csv_dataset)
   end
 
   test "should show dataset" do
-    @metadatum = metadata(:one)
-    get metadatum_dataset_url
+    get metadatum_dataset_url(@metadatum, @dataset)
+    assert_response :success
+  end
+
+  test "should download dataset csv" do
+    get download_metadatum_dataset_url(@metadatum, @dataset, format: @dataset.format.downcase)
+    assert_response :success
+  end
+
+  test "should download dataset geojson" do
+    @dataset = datasets(:geojson_dataset)
+    get download_metadatum_dataset_url(@metadatum, @dataset, format: @dataset.format.downcase)
     assert_response :success
   end
 end
