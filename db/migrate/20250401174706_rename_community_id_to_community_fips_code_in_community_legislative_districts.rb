3a48b5ad0c5cb6fdf00aa7400a15bfcbd71bf0fc
Alora Greer <agreer5@alaska.edu>
2025-04-15 15:25:18 -0800

    Fix community_fips_code migration: remove old FK, rename column, add correct FK

--- db/migrate/20250401174706_rename_community_id_to_community_fips_code_in_community_legislative_districts.rb
+++ db/migrate/20250401174706_rename_community_id_to_community_fips_code_in_community_legislative_districts.rb
@@ -1,9 +1,13 @@
 class RenameCommunityIdToCommunityFipsCodeInCommunityLegislativeDistricts < ActiveRecord::Migration[7.1]
   def change
-    rename_column :communities_legislative_districts, :community_id, :community_fips_code
     remove_foreign_key :communities_legislative_districts, :communities
 
+    rename_column :communities_legislative_districts, :community_id, :community_fips_code
+
     change_column :communities_legislative_districts, :community_fips_code, :string
-    add_foreign_key :communities_legislative_districts, :communities, column: :community_fips_code, primary_key: :fips_code
+
+    add_foreign_key :communities_legislative_districts, :communities,
+                    column: :community_fips_code,
+                    primary_key: :fips_code
   end
 end
