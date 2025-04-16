1a6e8843ac06cfe44a070eb9d7b15a91c2106249
Alora Greer <agreer5@alaska.edu>
2025-04-16 14:33:49 -0800

    Fix migration to add new `community_fips_code` column and populate it using `community_id`. Adds new FK, removes old FK and column. Defines `down` method to raise since migration is destructive.

--- db/migrate/20250401174706_rename_community_id_to_community_fips_code_in_community_legislative_districts.rb
+++ db/migrate/20250401174706_rename_community_id_to_community_fips_code_in_community_legislative_districts.rb
@@ -1,13 +1,28 @@
 class RenameCommunityIdToCommunityFipsCodeInCommunityLegislativeDistricts < ActiveRecord::Migration[7.1]
-  def change
-    remove_foreign_key :communities_legislative_districts, :communities
-
-    remove_column :communities_legislative_districts, :community_id, :community_fips_code
-
+  def up
     add_column :communities_legislative_districts, :community_fips_code, :string
+
+    # repopulate the community_fips_code column
+    if CommunitiesLegislativeDistrict.exists?
+      say_with_time "Repopulating community_fips_code from community_id..." do
+        CommunitiesLegislativeDistrict.reset_column_information
+
+        CommunitiesLegislativeDistrict.find_each do |cld|
+          community = Community.find_by(id: cld.community_id)
+          cld.update_column(:community_fips_code, community&.fips_code)
+        end
+      end
+    end
 
     add_foreign_key :communities_legislative_districts, :communities,
                     column: :community_fips_code,
                     primary_key: :fips_code
+
+    remove_foreign_key :communities_legislative_districts, column: :communities_fips_code
+    remove_column :communities_legislative_districts, :communities_fips_code
+  end
+
+  def down
+    raise ActiveRecord::IrreversibleMigration, 'This migration cannot be reverted because it destroys sensitive data.'
   end
 end
