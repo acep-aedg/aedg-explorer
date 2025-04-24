441ead7c2eb4fff019cd98c69291ae466cc410d0
Nicole Mah <nicolemah99@gmail.com>
2025-04-23 16:17:30 -0800

    Add AEDG History Timeline to about page

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -1,2 +1,26 @@
 module StaticPagesHelper
+  def aedg_history_timeline
+    [
+      {
+        title: "AEDG Version 3.0",
+        date: "2024–2025",
+        desc: "University partners ISER and ACEP develop and launch the revitalized AEDG with updated datasets and enhanced tools and a modernized interface."
+      },
+      {
+        title: "State Support",
+        date: "2024",
+        desc: "The Alaska Senate approved funding to revitalize the AEDG."
+      },
+      {
+        title: "AEDG Version 2.0",
+        date: "2017",
+        desc: "AEDG was redesigned by ______ in collaboration with Lawrence Berkeley National Laboratory (Berkeley Lab)."
+      },
+      {
+        title: "AEDG Version 1.0",
+        date: "2013",
+        desc: "The Alaska Energy Data Gateway (AEDG) was created by UAA's Institute of Social and Economic Research (ISER) and UAF's Alaska Center for Energy and Power (ACEP) to showcase energy and socioeconomic data for Alaska"
+      }
+    ]
+  end
 end
