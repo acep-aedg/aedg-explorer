f01f29361284e45b54c4351418e04ba3d4c010ca
Nicole Mah <nicolemah99@gmail.com>
2025-05-01 15:27:45 -0800

    Remove footer helpers returning text, move into footer partial

--- app/helpers/footer_helper.rb
+++ app/helpers/footer_helper.rb
@@ -7,20 +7,4 @@ module FooterHelper
       { name: 'User Guide', path: user_guide_path, title: 'User Guide' }
     ]
   end
-
-  def about_title
-    'Alaska Energy Data Gateway'
-  end
-
-  def about_text
-    'The Alaska Energy Data Gateway is a public resource funded by the State of Alaska to ensure energy and socioeconomic data is easily accessible.'
-  end
-
-  def contact_email
-    '[AEDG_EMAIL]@alaska.edu'
-  end
-
-  def copyright_text
-    "© #{Time.current.year} Alaska Center for Energy and Power. All rights reserved."
-  end
 end
