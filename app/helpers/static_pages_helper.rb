88b821a828f88212a261e92b7a7ffac6e586100f
Nicole Mah <nicolemah99@gmail.com>
2025-05-12 15:09:36 -0800

    AEDG-438 Update site citation to include ISER

--- app/helpers/static_pages_helper.rb
+++ app/helpers/static_pages_helper.rb
@@ -55,6 +55,6 @@ module StaticPagesHelper
 
   def aedg_citation
     access_date = Time.zone.today.strftime('%B %-d, %Y')
-    "Alaska Center for Energy and Power. \"Alaska Energy Data Gateway v3.0.\" Accessed #{access_date}. https://akenergygateway.alaska.edu/"
+    "Alaska Center for Energy and Power (ACEP) and Institute of Social and Economic Research (ISER). \"Alaska Energy Data Gateway v3.0.\" Accessed #{access_date}. https://akenergygateway.alaska.edu/"
   end
 end
