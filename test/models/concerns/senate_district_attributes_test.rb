f34df94cbc9eaebebcd60041315dbf8e07d92afa
Nicole Mah <nicolemah99@gmail.com>
2025-05-19 10:47:05 -0800

    AEDG-402 Add tests for senate_districts_attributes, change test geom from polygon to multipolygon

--- test/models/concerns/senate_district_attributes_test.rb
+++ test/models/concerns/senate_district_attributes_test.rb
@@ -1,11 +1,11 @@
 require 'test_helper'
 require 'rgeo'
 
-class HouseDistrictAttributesTest < ActiveSupport::TestCase
+class SenateDistrictAttributesTest < ActiveSupport::TestCase
   def setup
     @geom_factory = RGeo::Geographic.simple_mercator_factory
 
-    @polygon_geom = @geom_factory.polygon(
+    polygon = @geom_factory.polygon(
       @geom_factory.linear_ring([
                                   @geom_factory.point(0, 0),
                                   @geom_factory.point(0, 1),
@@ -15,22 +15,22 @@ class HouseDistrictAttributesTest < ActiveSupport::TestCase
                                 ])
     )
 
+    @polygon_geom = @geom_factory.multi_polygon([polygon])
+
     @valid_props = {
-      'name' => 'Test House District',
-      'district' => 1000,
-      'as_of_date' => '2023-01-01'
+      'district' => 'ABC',
+      'as_of_date' => '2022-01-01'
     }
   end
 
-  test 'creates a new house district with geometry and attributes' do
-    house_district = nil
-    assert_difference -> { HouseDistrict.count }, +1 do
-      house_district = HouseDistrict.import_aedg_with_geom!(@valid_props, @polygon_geom)
+  test 'creates a new senate district with geometry and attributes' do
+    senate_district = nil
+    assert_difference -> { SenateDistrict.count }, +1 do
+      senate_district = SenateDistrict.import_aedg_with_geom!(@valid_props, @polygon_geom)
     end
-    assert_equal @valid_props[:name], house_district.name
-    assert_equal @valid_props[:district], house_district.district
-    assert_equal Date.parse(@valid_props[:as_of_date]), house_district.as_of_date
-    assert_equal @polygon_geom.as_text, house_district.boundary.as_text
+    assert_equal @valid_props[:district], senate_district.district
+    assert_equal Date.parse(@valid_props[:as_of_date]), senate_district.as_of_date
+    assert_equal @polygon_geom.as_text, senate_district.boundary.as_text
   end
 
   test 'is invalid with incorrect geometry type' do
@@ -40,7 +40,7 @@ class HouseDistrictAttributesTest < ActiveSupport::TestCase
                                      ])
 
     assert_raises ActiveRecord::RecordInvalid do
-      HouseDistrict.import_aedg_with_geom!(@valid_props, line)
+      SenateDistrict.import_aedg_with_geom!(@valid_props, line)
     end
   end
 end
