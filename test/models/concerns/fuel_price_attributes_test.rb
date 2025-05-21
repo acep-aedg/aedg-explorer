8bdb010b294f1f1ff69727eac7ee9eb6c63cccd2
Nicole Mah <nicolemah99@gmail.com>
2025-05-21 14:12:53 -0800

    AEDG-419 Add tests for fuel_price_attributes, add uniqueness validation for community,reporting season, reporting year

--- test/models/concerns/fuel_price_attributes_test.rb
+++ test/models/concerns/fuel_price_attributes_test.rb
@@ -9,43 +9,41 @@ class FuelPriceAttributesTest < ActiveSupport::TestCase
     @community.save(validate: false)
 
     @valid_props = {
-      community_fips_code: @community.fips_code,
-      price: 2.5,
-      fuel_type: 'Test Fuel Type',
-      price_type: 'Test Price Type',
-      source: 'Test Source',
+      community_fips_code: VALID_FIPS_CODE,
       reporting_season: 'Test Reporting Season',
       reporting_year: 2020
     }
   end
 
-  test 'creates a new fuel price record' do
+  test 'import_aedg! creates a fuel price record with valid props' do
     fp = nil
     assert_difference -> { FuelPrice.count }, +1 do
       fp = FuelPrice.import_aedg!(@valid_props)
     end
 
     assert_equal @community, fp.community
-    assert_equal @valid_props[:price], fp.price
-    assert_equal @valid_props[:fuel_type], fp.fuel_type
-    assert_equal @valid_props[:price_type], fp.price_type
-    assert_equal @valid_props[:source], fp.source
     assert_equal @valid_props[:reporting_season], fp.reporting_season
     assert_equal @valid_props[:reporting_year], fp.reporting_year
   end
 
-  test 'does not create a duplicate fuel price for the same community' do
+  test 'import_aedg! raises RecordInvalid for duplicate fuel price' do
     FuelPrice.import_aedg!(@valid_props)
-
-    assert_no_difference -> { Transportation.count } do
+    assert_raises(ActiveRecord::RecordInvalid) do
       FuelPrice.import_aedg!(@valid_props)
     end
   end
 
-  test 'raises error if community fips code does not match any community' do
-    props_with_invalid_fips = @valid_props.merge(community_fips_code: INVALID_FIPS_CODE)
+  test 'import_aedg! raises RecordInvalid when community fips code does not match any existing community' do
+    invalid_props = @valid_props.merge(community_fips_code: INVALID_FIPS_CODE)
     assert_raises(ActiveRecord::RecordInvalid) do
-      FuelPrice.import_aedg!(props_with_invalid_fips)
+      FuelPrice.import_aedg!(invalid_props)
+    end
+  end
+
+  test 'import_aedg! raises RecordInvalid when community fips code is nil' do
+    invalid_props = @valid_props.merge(community_fips_code: nil)
+    assert_raises(ActiveRecord::RecordInvalid) do
+      FuelPrice.import_aedg!(invalid_props)
     end
   end
 end
