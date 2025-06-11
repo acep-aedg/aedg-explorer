4fcca3ac27bd1893b160b0b7bc18c2d5e67fe63e
Alora Greer <agreer5@alaska.edu>
2025-06-10 16:12:28 -0800

    make fuel prices chart more user friendly. created categoires to track nils to diplay on chart. changes summer and winter to green and blue. added dropdown to switch between survey and regional price type to be more transparant. since x-axis bar grouped by year there is one tooltip per group for chartkick.

--- app/helpers/communities/charts_helper.rb
+++ app/helpers/communities/charts_helper.rb
@@ -1,4 +1,5 @@
 module Communities::ChartsHelper
+  require 'bigdecimal'
   # structured for chartkick
   def employment_chart_data(employments)
     [
@@ -67,31 +68,27 @@ module Communities::ChartsHelper
     }
   end
 
-  def fuel_prices_chart_data(fuel_prices)
-    fuel_prices
-      .group_by(&:fuel_type)
-      .map do |fuel_type, recs|
-        {
-          name: fuel_type.humanize,
-          data: recs
-            .sort_by { |r| [r.reporting_year, r.reporting_season] }
-            .each_with_object({}) do |r, h|
-              h["#{r.reporting_year} #{r.reporting_season}"] = r.price.to_f
-            end
-        }
-      end
-  end
+  def fuel_prices_by_season_chart_data(fuel_prices, price_type: nil)
+    filtered = fuel_prices.select { |fp| fp.price_type.to_s.downcase == price_type.to_s.downcase }
 
-  def fuel_prices_by_season_chart_data(fuel_prices)
-    fuel_prices
-      .group_by(&:reporting_season)
-      .map do |season, recs|
-        {
-          name: season,
-          data: recs
-            .group_by(&:reporting_year)
-            .transform_values { |a| a.sum { |fp| fp.price.to_f } }
-        }
-      end
+    return [] if filtered.empty?
+
+    years = filtered.map(&:reporting_year).uniq.sort
+
+    # Fixed category order ensures consistent series across years
+    categories = ["Winter Gasoline", "Winter Heating Fuel", "Summer Gasoline", "Summer Heating Fuel"]
+
+    # Lookup: [year, category] => price Ex: [2024, "Winter Gasoline"] => 6.94
+    price_map = filtered.each_with_object({}) do |fp, hash|
+      label = "#{fp.reporting_season.to_s.capitalize} #{fp.fuel_type.to_s.titleize}"
+      hash[[fp.reporting_year, label]] = BigDecimal(fp.price.to_s)
+    end
+
+    categories.map do |label|
+      {
+        name: label,
+        data: years.index_with { |year| price_map[[year, label]] }
+      }
+    end
   end
 end
