6702d7e12f06b7976c4fa6370cbb1dbec65cd249
Nicole Mah <nicolemah99@gmail.com>
2025-06-12 11:54:51 -0800

    Add nav_link helper method to apply DRY principles

--- app/helpers/communities_helper.rb
+++ app/helpers/communities_helper.rb
@@ -1,4 +1,8 @@
 module CommunitiesHelper
+  def nav_link(text, anchor, parent_section: false)
+    link_to text, anchor.to_s, class: "nav-link #{parent_section ? 'fw-semibold' : 'ps-4'} py-1", data: { turbo: false }
+  end
+
   # Electricity section visibility methods
   def show_utilities?(community)
     community.reporting_entity.present?
