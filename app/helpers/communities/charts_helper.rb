7a69cefd1366e996e4b0504becb2f6c55497c52a
Nicole Mah <nicolemah99@gmail.com>
2025-06-13 13:40:14 -0800

    Undo attempted quick fix for BigDecimal bug

--- app/helpers/communities/charts_helper.rb
+++ app/helpers/communities/charts_helper.rb
@@ -81,15 +81,7 @@ module Communities::ChartsHelper
     # Lookup: [year, category] => price Ex: [2024, "Winter Gasoline"] => 6.94
     price_map = filtered.each_with_object({}) do |fp, hash|
       label = "#{fp.reporting_season.to_s.capitalize} #{fp.fuel_type.to_s.titleize}"
-      price = fp.price.to_s
-      next if price.blank?
-
-      begin
-        hash[[fp.reporting_year, label]] = BigDecimal(price)
-      rescue ArgumentError
-        Rails.logger.warn "Invalid price '#{price}' for fuel_price ##{fp.id}"
-        next
-      end
+      hash[[fp.reporting_year, label]] = BigDecimal(fp.price.to_s)
     end
 
     categories.map do |label|
