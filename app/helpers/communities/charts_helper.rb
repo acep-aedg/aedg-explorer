6ab9d77ab99f7dfb8bfebae441e00b93c1f3916b
Nicole Mah <nicolemah99@gmail.com>
2025-06-13 13:20:02 -0800

    Fix rubocop

--- app/helpers/communities/charts_helper.rb
+++ app/helpers/communities/charts_helper.rb
@@ -81,7 +81,15 @@ module Communities::ChartsHelper
     # Lookup: [year, category] => price Ex: [2024, "Winter Gasoline"] => 6.94
     price_map = filtered.each_with_object({}) do |fp, hash|
       label = "#{fp.reporting_season.to_s.capitalize} #{fp.fuel_type.to_s.titleize}"
-      hash[[fp.reporting_year, label]] = BigDecimal(fp.price.to_s)
+      price = fp.price.to_s
+      next if price.blank?
+
+      begin
+        hash[[fp.reporting_year, label]] = BigDecimal(price)
+      rescue ArgumentError
+        Rails.logger.warn "Invalid price '#{price}' for fuel_price ##{fp.id}"
+        next
+      end
     end
 
     categories.map do |label|
