07b5ad23d9b1771b9e12568d6bc02496fe3550d3
Nicole Mah <nicolemah99@gmail.com>
2025-09-02 08:57:41 -0800

    Rename community-dropdown controller to dropdown, reorder quick nav, add text for context

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,6 +1,6 @@
 import { Controller } from '@hotwired/stimulus';
 
-// Connects to data-controller="community-dropdown"
+// Connects to data-controller="dropdown"
 export default class extends Controller {
   static targets = ['select'];
 
