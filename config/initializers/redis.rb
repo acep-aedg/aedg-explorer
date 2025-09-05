2061078d2a61bd36b0f874c19d900935b5e47b7e
Alora Greer <agreer5@alaska.edu>
2025-09-05 13:37:10 -0800

    do not seperate pass from url use passed in env of url w pass

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,9 +1,8 @@
 require 'connection_pool'
 require 'redis'
 
-url      = ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/0')
-password = ENV['REDIS_PASSWORD']
+url = ENV.fetch('REDIS_URL', 'redis://127.0.0.1:6379/0')
 
 REDIS_POOL = ConnectionPool.new(size: Integer(ENV.fetch('RAILS_MAX_THREADS', 5)), timeout: 5) do
-  Redis.new(url: url, password: password)
+  Redis.new(url: url)
 end
