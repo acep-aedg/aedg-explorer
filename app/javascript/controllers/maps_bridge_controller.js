dffaf14d060257a12b51dd2b6c5ab21b62a00f4b
Alora Greer <agreer5@alaska.edu>
2025-10-07 08:59:56 -0800

    remove autoscrool on map for communities leg and gird utilies. add defuralt map on grids to show communities when going to the page. Noticed a bug with the map layer on gird page and will look into on next task

--- app/javascript/controllers/maps_bridge_controller.js
+++ app/javascript/controllers/maps_bridge_controller.js
@@ -30,31 +30,6 @@ export default class extends Controller {
     };
 
     this.element.addEventListener("click", this._onClick);
-
-    // --- Observe the header to auto-select default once
-    if (this.hasTriggerTarget && this.hasDefaultLayerUrlValue) {
-      const root = this.rootIdValue ? document.getElementById(this.rootIdValue) : null;
-      this._seen = false;
-      this._io = new IntersectionObserver(
-        (entries) => {
-          const entry = entries[0];
-          if (!entry.isIntersecting) return;
-          if (Date.now() < this._manualUntil) return;
-          if (this._seen) return;
-
-          this._seen = true;
-          const detail = {
-            url: this.defaultLayerUrlValue,
-            color: this.defaultColorValue,
-            outlineColor: this.defaultOutlineColorValue,
-            clear: true,
-          };
-          window.dispatchEvent(new CustomEvent("maps:select-layer", { detail }));
-        },
-        { root, threshold: 0.5 }
-      );
-      this._io.observe(this.triggerTarget);
-    }
   }
 
   disconnect() {
