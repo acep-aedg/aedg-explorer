7a94f6d134f5c1afd8bd74ef4125a517c3c21bd4
Nicole Mah <nicolemah99@gmail.com>
2025-11-10 14:38:20 -0900

    Move map controller json logic into jbuilder files

--- app/controllers/communities/maps_controller.rb
+++ app/controllers/communities/maps_controller.rb
@@ -2,67 +2,12 @@ module Communities
   class MapsController < ApplicationController
     before_action :set_community
 
-    def house_districts
-      geojson = Rails.cache.fetch(['maps', @community.cache_key_with_version, @community.house_districts.cache_key_with_version], expires_in: 12.hours) do
-        {
-          type: 'FeatureCollection',
-          features: @community.house_districts.map(&:as_geojson)
-        }
-      end
-
-      render json: geojson
-    end
-
-    def senate_districts
-      geojson = Rails.cache.fetch(['maps', @community.cache_key_with_version, @community.senate_districts.cache_key_with_version], expires_in: 12.hours) do
-        {
-          type: 'FeatureCollection',
-          features: @community.senate_districts.map(&:as_geojson)
-        }
-      end
-
-      render json: geojson
-    end
-
-    def service_area_geoms
-      geojson = Rails.cache.fetch(['maps', @community.cache_key_with_version, @community.service_area_geoms.cache_key_with_version], expires_in: 12.hours) do
-        {
-          type: 'FeatureCollection',
-          features: @community.service_area_geoms.map(&:as_geojson)
-        }
-      end
-      render json: geojson
-    end
-
-    def service_areas
-      geojson = Rails.cache.fetch(['maps', @community.cache_key_with_version, @community.service_areas.cache_key_with_version], expires_in: 12.hours) do
-        {
-          type: 'FeatureCollection',
-          features: @community.service_areas.map(&:as_geojson)
-        }
-      end
-      render json: geojson
-    end
-
-    def plants
-      geojson = Rails.cache.fetch(['maps', @community.cache_key_with_version, @community.plants.cache_key_with_version], expires_in: 12.hours) do
-        {
-          type: 'FeatureCollection',
-          features: @community.plants.map(&:as_geojson)
-        }
-      end
-      render json: geojson
-    end
-
-    def bulk_fuel_facilities
-      geojson = Rails.cache.fetch(['maps', @community.cache_key_with_version, 'bulk_fuel_facilities'], expires_in: 12.hours) do
-        {
-          type: 'FeatureCollection',
-          features: @community.bulk_fuel_facilities.map(&:as_geojson)
-        }
-      end
-      render json: geojson
-    end
+    def house_districts; end
+    def senate_districts; end
+    def service_area_geoms; end
+    def service_areas; end
+    def plants; end
+    def bulk_fuel_facilities; end
 
     private
 
--- app/controllers/grids/maps_controller.rb
+++ app/controllers/grids/maps_controller.rb
@@ -2,30 +2,8 @@ module Grids
   class MapsController < ApplicationController
     before_action :set_grid
 
-    def community_locations
-      geojson = Rails.cache.fetch(['maps', @grid.cache_key_with_version, @grid.communities.cache_key_with_version], expires_in: 12.hours) do
-        {
-          type: 'FeatureCollection',
-          features: @grid.communities.with_location.map do |c|
-            feat = c.as_geojson
-            feat[:properties][:path] = helpers.community_path(c)
-            feat
-          end
-        }
-      end
-
-      render json: geojson
-    end
-
-    def service_area_geoms
-      geojson = Rails.cache.fetch(['maps', @grid.cache_key_with_version, @grid.service_area_geoms.cache_key_with_version], expires_in: 12.hours) do
-        {
-          type: 'FeatureCollection',
-          features: @grid.service_area_geoms.distinct.map(&:as_geojson)
-        }
-      end
-      render json: geojson
-    end
+    def community_locations; end
+    def service_area_geoms; end
 
     private
 
