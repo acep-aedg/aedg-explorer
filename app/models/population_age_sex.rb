6701963b785e3844aabbd7698e09efbaa48d5aa3
Nicole Mah <nicolemah99@gmail.com>
2025-11-17 15:00:40 -0900

    fix git

--- app/models/population_age_sex.rb
+++ app/models/population_age_sex.rb
@@ -6,15 +6,8 @@ class PopulationAgeSex < ApplicationRecord
   scope :for_community, ->(community) { where(community_fips_code: community.fips_code) }
 
   # Returns the most recent record for this community,
-  # ensuring all fields in the required_group are NOT NULL (if provided).
-  def self.most_recent_for(community, required_fields: nil)
-    rel = for_community(community)
-
-    Array(required_fields).each do |field|
-      rel = rel.where.not(field => nil)
-    end
-
-    rel.order(end_year: :desc).first
+  def self.most_recent_for(community)
+    for_community(community).order(end_year: :desc).first
   end
 
   # Scope to order by start year if needed
