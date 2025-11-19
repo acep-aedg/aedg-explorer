425a13913203b14d7a36b3d8a62e9f5e003af046
Nicole Mah <nicolemah99@gmail.com>
2025-11-18 15:03:38 -0900

    Add documentation

--- app/javascript/maps/config.js
+++ app/javascript/maps/config.js
@@ -1,10 +1,10 @@
 export const MAP_STYLE = "mapbox://styles/mapbox/streets-v11?optimize=true";
 export const DEFAULT_ZOOM = 8;
 export const LAYER_COLORS = {
-  "layer-communities": "#75ba75",
-  "layer-plants": "#088",
-  "layer-senate": "#fec76f",
-  "layer-house": "#6dbfb8",
+  "layer-communities": "#EE4B2B",
+  "layer-plants": "#f5945c",
+  "layer-senate": "#e67e22",
+  "layer-house": "#088",
   "layer-service-area-local": "#E74C3C",
   "layer-service-area-full": "#E74C3C",
   "layer-bulk-fuel-facilities": "#786a39",
