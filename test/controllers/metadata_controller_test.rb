886f0c8c62cc96034b8f76aa73c4d9260f513327
Nicole Mah <nicolemah99@gmail.com>
2025-12-22 14:17:00 -0900

    Add new tests for rails_blob_path and zipping and attaching to dataset, restructure dataset fixture files to replicate new datapond structure

--- test/controllers/metadata_controller_test.rb
+++ test/controllers/metadata_controller_test.rb
@@ -1,4 +1,5 @@
 require 'test_helper'
+require 'zip'
 
 class MetadataControllerTest < ActionDispatch::IntegrationTest
   setup do
@@ -23,5 +24,41 @@ class MetadataControllerTest < ActionDispatch::IntegrationTest
   test 'should download metadatum' do
     get download_metadatum_url(@metadatum)
     assert_response :success
+  end
+
+  test 'should download metadatum/dataset archive via active storage' do
+    dataset = @metadatum.dataset
+    attach_virtual_zip(dataset)
+
+    download_path = Rails.application.routes.url_helpers.rails_blob_path(
+      dataset.archive,
+      disposition: 'attachment',
+      only_path: true
+    )
+
+    get download_path
+
+    assert_response :redirect
+
+    follow_redirect!
+    assert_response :success
+    assert_match(/attachment/, response.headers['Content-Disposition'])
+  end
+
+  private
+
+  # Helper to generate a valid zip in memory and attach it
+  def attach_virtual_zip(dataset)
+    stringio = Zip::OutputStream.write_buffer do |zio|
+      zio.put_next_entry('test.txt')
+      zio.write 'This is a test text file'
+    end
+    stringio.rewind
+
+    dataset.archive.attach(
+      io: stringio,
+      filename: 'test_archive.zip',
+      content_type: 'application/zip'
+    )
   end
 end
