a8a5ca061cce2719ee141dfcc1619c99ad1322d0
Nicole Mah <nicolemah99@gmail.com>
2026-01-16 10:22:14 -0900

    Add back communities_legislative_district model to ensure migrations run properly

--- app/models/communities_legislative_district.rb
+++ app/models/communities_legislative_district.rb
@@ -1,6 +1,3 @@
 class CommunitiesLegislativeDistrict < ApplicationRecord
-  include CommunitiesLegislativeDistrictAttributes
-  belongs_to :community, foreign_key: 'community_fips_code', primary_key: 'fips_code'
-  belongs_to :house_district, dependent: :destroy
-  belongs_to :senate_district, dependent: :destroy
+  # This model/table has been dropped, keep this file to ensure migrations run without error
 end
--- test/models/communities_legislative_district_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CommunitiesLegislativeDistrictTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
