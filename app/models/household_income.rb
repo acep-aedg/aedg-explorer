1358dea929cc5af05efb36d48d4913906bae563e
Nicole Mah <nicolemah99@gmail.com>
2026-01-27 17:19:51 -0900

    Check any income fields exists with valid data before showing the income distribution chart

--- app/models/household_income.rb
+++ app/models/household_income.rb
@@ -53,4 +53,8 @@ class HouseholdIncome < ApplicationRecord
   def self.show_median_income_chart?
     where("e_household_median_income > 0").exists?
   end
+
+  def self.show_income_distribution_chart?
+    where(INCOME_BRACKET_FIELDS.map { |field| "#{field} > 0" }.join(" OR ")).exists?
+  end
 end
