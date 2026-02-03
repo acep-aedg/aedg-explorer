1c5241fff36797b40360ec8e87945358beb47b20
Alora Greer <agreer5@alaska.edu>
2026-02-03 09:14:25 -0900

    update colors for map layers

--- app/javascript/maps/config.js
+++ app/javascript/maps/config.js
@@ -1,11 +1,25 @@
 export const MAP_STYLE = "mapbox://styles/mapbox/streets-v11?optimize=true";
 export const DEFAULT_ZOOM = 8;
+const COLORS = {
+  dark_blue:    "#2E5A88",
+  orange:       "#f5945c",
+  soft_green:   "#78c2ad",
+  dark_red:     "#EE4B2B",
+  light_blue:   "#3498db",
+  light_brown:  "#a98d60",
+  dark_green:   "#2d572c",
+  light_red:    "#E74C3C",
+  medium_yellow: "#f1c40f",
+  dark_brown:   "#786a39",
+  purple:       "#563978"
+};
+
 export const LAYER_COLORS = {
-  "layer-communities": "#EE4B2B",
-  "layer-plants": "#f5945c",
-  "layer-senate": "#e67e22",
-  "layer-house": "#088",
-  "layer-service-area-local": "#E74C3C",
-  "layer-service-area-full": "#E74C3C",
-  "layer-bulk-fuel-facilities": "#786a39",
+  "layer-communities":           COLORS.dark_blue,
+  "layer-plants":                COLORS.orange,
+  "layer-senate":                COLORS.purple,
+  "layer-house":                 COLORS.light_red,
+  "layer-service-area-local":    COLORS.dark_blue,
+  "layer-service-area-full":     COLORS.medium_yellow,
+  "layer-bulk-fuel-facilities":  COLORS.dark_green,
 };
