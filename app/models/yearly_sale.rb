3690a93a9b5adfa2a61885c5cdf095cd5d2a32a2
Nicole Mah <nicolemah99@gmail.com>
2026-03-06 13:51:41 -0900

    Hide fields with all NULL values in data tables

--- app/models/yearly_sale.rb
+++ app/models/yearly_sale.rb
@@ -22,20 +22,16 @@ class YearlySale < ApplicationRecord
     %i[residential commercial industrial transportation community government unbilled other]
   end
 
-  def self.categories
-    %i[sales revenue customers]
-  end
-
   def self.sales_fields
-    %i[residential_sales_mwh commercial_sales_mwh industrial_sales_mwh transportation_sales_mwh community_sales_mwh government_sales_mwh unbilled_sales_mwh other_sales_mwh]
+    %i[total_sales_mwh residential_sales_mwh commercial_sales_mwh industrial_sales_mwh transportation_sales_mwh community_sales_mwh government_sales_mwh unbilled_sales_mwh other_sales_mwh]
   end
 
   def self.revenue_fields
-    %i[residential_revenue commercial_revenue industrial_revenue transportation_revenue community_revenue government_revenue unbilled_revenue other_revenue]
+    %i[residential_revenue commercial_revenue industrial_revenue transportation_revenue community_revenue other_revenue total_revenue]
   end
 
   def self.customer_fields
-    %i[residential_customers commercial_customers industrial_customers transportation_customers community_customers government_customers unbilled_customers other_customers]
+    %i[total_customers residential_customers commercial_customers industrial_customers transportation_customers community_customers government_customers unbilled_customers other_customers]
   end
 
   def self.total_fields
@@ -44,6 +40,18 @@ class YearlySale < ApplicationRecord
 
   def self.available_years_for(owner)
     for_owner(owner).where.not(year: nil).distinct.order(year: :desc).pluck(:year)
+  end
+
+  def self.active_fields_for(category)
+    fields = case category.to_sym
+             when :sales     then sales_fields
+             when :revenue   then revenue_fields
+             when :customers then customer_fields
+             else []
+             end
+
+    active = fields.select { |field| where.not(field => nil).exists? }
+    active.sort_by { |f| f.to_s.include?("total") ? 0 : 1 }
   end
 
   def self.usage_per_customer_rates
