7ab675c553ca2946ee5ba37c062be2bbf50d19bf
Nicole Mah <nicolemah99@gmail.com>
2026-03-12 11:45:32 -0800

    Fix Typos and remove unused partial missed in merge conflict

--- app/helpers/communities_helper.rb
+++ app/helpers/communities_helper.rb
@@ -3,7 +3,7 @@ module CommunitiesHelper
     [
       { label: "General", path: general_community_path(community), visible: true },
       { label: "Power Generation", path: power_generation_community_path(community), visible: community.show_power_generation_tab? },
-      { label: "Rates & Sales", path: electric_rates_sales_community_path(community), visible: community.show_sales_rates_tab? },
+      { label: "Electricity Rates & Sales", path: electric_rates_sales_community_path(community), visible: community.show_sales_rates_tab? },
       { label: "Fuel", path: fuel_community_path(community), visible: community.show_fuel_tab? },
       { label: "Demographics", path: demographics_community_path(community), visible: community.show_demographics_tab? },
       { label: "Income", path: income_community_path(community), visible: community.show_income? }
