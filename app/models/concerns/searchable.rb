e1528aecd2afdaeb6ea6c100ccf43c46a7b2f911
Nicole Mah <nicolemah99@gmail.com>
2026-04-13 15:41:16 -0800

    Fix brakeman sql injection

--- app/models/concerns/searchable.rb
+++ app/models/concerns/searchable.rb
@@ -4,12 +4,12 @@ module Searchable
   included do
     class_attribute :searchable_column, default: :name
 
-    scope :starts_with, ->(letter) { where("#{searchable_column} ILIKE ?", "#{letter}%") if letter.present? }
+    scope :starts_with, ->(letter) { where(arel_table[searchable_column].matches("#{letter}%")) if letter.present? }
   end
 
   class_methods do
     def search_related(query)
-      where("#{searchable_column} ILIKE ?", "%#{query}%")
+      where(arel_table[searchable_column].matches("%#{query}%"))
     end
   end
 end
