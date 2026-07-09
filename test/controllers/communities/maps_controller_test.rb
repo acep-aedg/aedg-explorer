b570e2ccfcff278770f5f043f128180686d3777b
Nicole Mah <nicolemah99@gmail.com>
2026-07-09 10:07:06 -0800

    Update community maps tests

--- test/controllers/communities/maps_controller_test.rb
+++ test/controllers/communities/maps_controller_test.rb
@@ -9,13 +9,43 @@ module Communities
     test "should get house_districts" do
       get house_districts_community_maps_url(@community)
       assert_response :success
-      assert_equal "application/json", @response.media_type
+      assert_equal "application/geo+json", @response.media_type
     end
 
     test "should get senate_districts" do
       get senate_districts_community_maps_url(@community)
       assert_response :success
-      assert_equal "application/json", @response.media_type
+      assert_equal "application/geo+json", @response.media_type
+    end
+
+    test "should get service_area_geom" do
+      get service_area_geom_community_maps_url(@community)
+      assert_response :success
+      assert_equal "application/geo+json", @response.media_type
+    end
+
+    test "should get service_area" do
+      get service_area_community_maps_url(@community)
+      assert_response :success
+      assert_equal "application/geo+json", @response.media_type
+    end
+
+    test "should get plants" do
+      get plants_community_maps_url(@community)
+      assert_response :success
+      assert_equal "application/geo+json", @response.media_type
+    end
+
+    test "should get bulk_fuel_facilities" do
+      get bulk_fuel_facilities_community_maps_url(@community)
+      assert_response :success
+      assert_equal "application/geo+json", @response.media_type
+    end
+
+    test "should get boroughs" do
+      get boroughs_community_maps_url(@community)
+      assert_response :success
+      assert_equal "application/geo+json", @response.media_type
     end
   end
 end
