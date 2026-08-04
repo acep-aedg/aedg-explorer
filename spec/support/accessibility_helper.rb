932bd711ea4d968877eb39add75a59f6e16e0948
Nicole Mah <nicolemah99@gmail.com>
2026-08-04 15:36:26 -0800

    debug window size

--- spec/support/accessibility_helper.rb
+++ spec/support/accessibility_helper.rb
@@ -1,16 +1,10 @@
 module AccessibilityHelpers
   def expect_page_to_be_accessible(debug_versions: false)
     if debug_versions
-      caps = page.driver.browser.capabilities
-      puts "\n=== Test Environment Versions ==="
-      puts "Capybara:   #{Capybara::VERSION}"
-      puts "Selenium:   #{Selenium::WebDriver::VERSION}"
-      puts "Browser:    #{caps.browser_name} #{caps.browser_version}"
+      window_size = page.current_window.size # Gets an array like [1920, 1080]
       
-      # Extract ChromeDriver version from the Chrome capabilities hash
-      if caps["chrome"] && caps["chrome"]["chromedriverVersion"]
-        puts "Driver:     ChromeDriver #{caps['chrome']['chromedriverVersion'].split(' ').first}"
-      end
+      puts "\n=== Test Environment Details ==="
+      puts "Window Size: #{window_size[0]}x#{window_size[1]}"
       puts "=================================\n"
     end
     standards = %i[wcag2a wcag2aa wcag21a wcag21aa]
