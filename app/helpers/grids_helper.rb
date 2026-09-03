f32206eeb6d3c33ca6b713a9f6a5f1fcc895601e
Nicole Mah <nicolemah99@gmail.com>
2026-09-03 10:04:05 -0800

    Adjust colors, attributes for accessibility

--- app/helpers/grids_helper.rb
+++ app/helpers/grids_helper.rb
@@ -1,11 +1,11 @@
 module GridsHelper
   def grid_status_badge(community_grid)
-    return tag.span("Prior to #{community_grid.termination_year}", class: "badge bg-light text-secondary border ms-2") unless community_grid.active?
+    return tag.span("Prior to #{community_grid.termination_year}", class: "badge bg-light text-muted border ms-2") unless community_grid.active?
 
     if community_grid.connection_year.to_i.positive?
-      tag.span("Since #{community_grid.connection_year}", class: "badge bg-success-subtle text-success border border-success-subtle ms-2")
+      tag.span("Since #{community_grid.connection_year}", class: "badge bg-success-subtle text-success-emphasis border border-success-subtle ms-2")
     else
-      tag.span("Current", class: "badge bg-success-subtle text-success border border-success-subtle ms-2")
+      tag.span("Current", class: "badge bg-success-subtle text-success-emphasis border border-success-subtle ms-2")
     end
   end
 end
